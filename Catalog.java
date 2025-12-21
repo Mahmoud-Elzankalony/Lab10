2c16d6f3ce32d5469db650358e4013aff3bad2e4
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-12-21 16:39:43 +0200

    Create Catalog.java

---
+++ Catalog.java
@@ -0,0 +1,8 @@
+class Catalog
+{
+    // True if there is a game in progress, False otherwise.
+    boolean current;
+    // True if there is atleast one game available
+// for each difficulty, False otherwise.
+    boolean allModesExist;
+}
