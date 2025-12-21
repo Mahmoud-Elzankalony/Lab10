0482f6fb412770c3d053bee77bc1326e0ad300b4
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-12-21 16:37:34 +0200

    add game class

--- CreateLevels.java
+++ CreateLevels.java
@@ -4,7 +4,7 @@ public class CreateLevels {
     
     public void createLevels() throws IOException {
         Easy easy = new Easy();
-        Medium medium = new Medium();
+        //Medium medium = new Medium();
         Hard hard = new Hard();
     }
 
---
+++ Game.java
@@ -0,0 +1,11 @@
+class Game
+{
+    int[][] board;
+    Game(int[][] board)
+    {
+// IMPORTANT: DON'T COPY THE BOARD BY VALUE
+// USE REFERENCES
+        this.board = board;
+    }
+// Add methods and attributes if needed
+}
